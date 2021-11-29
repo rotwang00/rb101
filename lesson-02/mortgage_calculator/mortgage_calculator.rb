4573f8354b61955b1cba7d26ab7f749a8450a10c
Paul Thurmond <paulthurmond@gmail.com>
2021-11-29 05:54:16 -0600

    Ready for code review

--- lesson-02/mortgage_calculator/mortgage_calculator.rb
+++ lesson-02/mortgage_calculator/mortgage_calculator.rb
@@ -3,22 +3,20 @@ require "yaml"
 
 MESSAGES = YAML.load_file("mortgage_calculator_messages.yml")
 
-loan_duration_total_months = ""
-monthly_interest_rate = ""
-monthly_payment = ""
+loan_dur_tot_mon = ""
+mon_int_rate = ""
+mon_pmnt = ""
 
-def compute(
-    loan_amount,
-    loan_duration_years_part,
-    loan_duration_months_part,
-    apr
-  )
-  loan_duration_total_months = (loan_duration_years_part * 12) + loan_duration_months_part
-  monthly_interest_rate = apr / 12
-  monthly_payment = (loan_amount * ((monthly_interest_rate * 0.01) / (1 - (1 + (monthly_interest_rate * 0.01))**(-loan_duration_total_months))))
-  return loan_duration_total_months,
-    monthly_interest_rate,
-    monthly_payment
+def compute(loan_amt, loan_dur_yrs_part, loan_dur_mth_part, apr)
+  loan_dur_tot_mon = (loan_dur_yrs_part * 12) + loan_dur_mth_part
+  mon_int_rate = apr / 12
+
+  mon_pmnt = (loan_amt * ((mon_int_rate * 0.01) / (1 -
+    (1 + (mon_int_rate * 0.01))**(-loan_dur_tot_mon))))
+
+  return loan_dur_tot_mon,
+    mon_int_rate,
+    mon_pmnt
 end
 
 def prompt(key, custom_data="")
@@ -26,11 +24,11 @@ def prompt(key, custom_data="")
   Kernel.puts("=> #{message}")
 end
 
-def valid_number?(num)
+def valid_num?(num)
   num.to_f.to_s == num || num.to_i.to_s == num
 end
 
-def non_negative_number?(num)
+def non_neg_num?(num)
   num.to_f >= 0
 end
 
@@ -56,12 +54,12 @@ prompt(:welcome, name)
 # Main loop
 loop do
   # Get loan amount
-  loan_amount = ""
+  loan_amt = ""
   loop do
-    prompt(:loan_amount_prompt)
-    loan_amount = Kernel.gets().chomp()
+    prompt(:loan_amt_prompt)
+    loan_amt = Kernel.gets().chomp()
 
-    if valid_number?(loan_amount) && non_negative_number?(loan_amount)
+    if valid_num?(loan_amt) && non_neg_num?(loan_amt)
       break
     else
       prompt(:number_invalid)
@@ -74,7 +72,7 @@ loop do
     prompt(:apr_prompt)
     apr = Kernel.gets().chomp()
 
-    if valid_number?(apr) && non_negative_number?(apr)
+    if valid_num?(apr) && non_neg_num?(apr)
       break
     else
       prompt(:number_invalid)
@@ -82,12 +80,12 @@ loop do
   end
 
   # Get years part of term
-  loan_duration_years_part = ""
+  loan_dur_yrs_part = ""
   loop do
-    prompt(:loan_duration_years_prompt)
-    loan_duration_years_part = Kernel.gets().chomp()
+    prompt(:loan_dur_yrs_prompt)
+    loan_dur_yrs_part = Kernel.gets().chomp()
 
-    if valid_number?(loan_duration_years_part) && non_negative_number?(loan_duration_years_part)
+    if valid_num?(loan_dur_yrs_part) && non_neg_num?(loan_dur_yrs_part)
       break
     else
       prompt(:number_invalid)
@@ -95,12 +93,12 @@ loop do
   end
 
   # Get months part of term
-  loan_duration_months_part = ""
+  loan_dur_mth_part = ""
   loop do
-    prompt(:loan_duration_months_prompt)
-    loan_duration_months_part = Kernel.gets().chomp()
+    prompt(:loan_dur_mon_prompt)
+    loan_dur_mth_part = Kernel.gets().chomp()
 
-    if valid_number?(loan_duration_months_part) && non_negative_number?(loan_duration_months_part)
+    if valid_num?(loan_dur_mth_part) && non_neg_num?(loan_dur_mth_part)
       break
     else
       prompt(:number_invalid)
@@ -108,17 +106,20 @@ loop do
   end
 
   # Compute results
-  loan_duration_total_months, monthly_interest_rate, monthly_payment = compute(loan_amount.to_f, loan_duration_years_part.to_f, loan_duration_months_part.to_f, apr.to_f)
+  loan_dur_tot_mon, mon_int_rate, mon_pmnt = compute(loan_amt.to_f,
+                                                     loan_dur_yrs_part.to_f,
+                                                     loan_dur_mth_part.to_f,
+                                                     apr.to_f)
 
   # Display results
   system("clear") || system("cls")
 
   prompt(:results)
   puts "\n"
-  prompt(:loan_amount_result, format("%.2f", loan_amount))
-  prompt(:monthly_interest_rate_result, format("%.2f", monthly_interest_rate))
-  prompt(:loan_duration_months_result, loan_duration_total_months.to_i)
-  prompt(:monthly_payment_result, format("%.2f", monthly_payment))
+  prompt(:loan_amt_result, format("%.2f", loan_amt))
+  prompt(:mon_int_rate_result, format("%.2f", mon_int_rate))
+  prompt(:loan_dur_mon_result, loan_dur_tot_mon.to_i)
+  prompt(:mon_pmnt_result, format("%.2f", mon_pmnt))
   puts "\n"
 
   # Ask about going again
