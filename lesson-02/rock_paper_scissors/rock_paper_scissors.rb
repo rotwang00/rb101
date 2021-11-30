90b0d8d177f730d6fe42c3e054c0f48d33321a45
Paul Thurmond <paulthurmond@gmail.com>
2021-11-30 16:20:10 -0600

    Basic functionality

--- lesson-02/rock_paper_scissors/rock_paper_scissors.rb
+++ lesson-02/rock_paper_scissors/rock_paper_scissors.rb
@@ -4,30 +4,42 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-choice = ""
-loop do
-  prompt("Choose one: #{VALID_CHOICES.join(", ")}")
-  choice = Kernel.gets().chomp()
-
-  if VALID_CHOICES.include?(choice)
-    break
+def display_results(player, computer)
+  if (player == "rock" && computer == "scissors") ||
+    (player == "scissors" && computer == "paper") ||
+    (player == "paper" && computer == "rock")
+    prompt("You won!")
+  elsif (computer == "rock" && player == "scissors") ||
+    (computer == "scissors" && player == "paper") ||
+    (computer == "paper" && player == "rock")
+    prompt("Computer won!")
   else
-    prompt("That's not a valid choice.")
+    prompt("It's a tie!")
   end
 end
 
-computer_choice = VALID_CHOICES.sample()
+loop do
+  choice = ""
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(", ")}")
+    choice = Kernel.gets().chomp()
 
-Kernel.puts("You chose: #{choice}; Computer chose #{computer_choice}")
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
 
-if (choice == "rock" && computer_choice == "scissors") ||
-  (choice == "scissors" && computer_choice == "paper") ||
-  (choice == "paper" && computer_choice == "rock")
-  prompt("You won!")
-elsif (computer_choice == "rock" && choice == "scissors") ||
-  (computer_choice == "scissors" && choice == "paper") ||
-  (computer_choice == "paper" && choice == "rock")
-  prompt("Computer won!")
-else
-  prompt("It's a tie!")
+  computer_choice = VALID_CHOICES.sample()
+
+  prompt("You chose: #{choice}; Computer chose #{computer_choice}")
+
+  display_results(choice, computer_choice)
+
+  prompt("Do you want to play again?")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase.start_with?("y")
 end
+
+prompt("Thanks for playing. Goodbye!")
