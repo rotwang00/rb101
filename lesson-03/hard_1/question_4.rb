a74b053b44520a7557de56c02c6e7d00b1a5b2c4
Paul Thurmond <paulthurmond@gmail.com>
2021-12-06 11:47:40 -0600

    Finished Lesson 3

---
+++ lesson-03/hard_1/question_4.rb
@@ -0,0 +1,13 @@
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  if dot_separated_words.size != 4
+    return false
+  end
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    if !is_an_ip_number?(word)
+      return false
+    end
+  end
+  return true
+end
