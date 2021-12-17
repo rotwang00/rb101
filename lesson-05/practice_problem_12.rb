0653a5f975908cd0deb419222071c2b65a364b61
Paul Thurmond <paulthurmond@gmail.com>
2021-12-17 16:22:05 -0600

    Completed some Lesson 5 problems

---
+++ lesson-05/practice_problem_12.rb
@@ -0,0 +1,13 @@
+arr = [[:a, 1], ['b', 'two'], ['sea', {c: 3}], [{a: 1, b: 2, c: 3, d: 4}, 'D']]
+
+new_hash = {}
+
+arr.each do |elem|
+  new_hash[elem[0]] = elem[1]
+end
+
+p new_hash
+
+
+
+# expected return value: {:a=>1, "b"=>"two", "sea"=>{:c=>3}, {:a=>1, :b=>2, :c=>3, :d=>4}=>"D"}
