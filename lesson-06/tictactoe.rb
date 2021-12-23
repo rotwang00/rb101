5c39df7fb857be5f097d2fe69d59a34204ca2a44
Paul Thurmond <paulthurmond@gmail.com>
2021-12-23 07:43:13 -0600

    Added input to tic tac toe

--- lesson-06/tictactoe.rb
+++ lesson-06/tictactoe.rb
@@ -1,3 +1,11 @@
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
 def display_board(brd)
   puts ""
   puts "     |     |"
@@ -15,11 +23,29 @@ def display_board(brd)
 end
 
 def initialize_board
-  new_board = []
-  (1..9).each { |num| new_board[num] = ' ' }
+  new_board = {}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
+end
+
+def empty_squares(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+def player_places_piece!(brd)
+  square = ''
+  loop do
+    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    prompt "Sorry, that's not a valid choice."
+  end
+  brd[square] = PLAYER_MARKER
 end
 
 board = initialize_board
 
+display_board(board)
+
+player_places_piece!(board)
 display_board(board)
