739fc1125e48c02c2dd5132497747cbb4b003c5a
Paul Thurmond <paulthurmond@gmail.com>
2021-12-29 16:25:50 -0600

    Can play a round

--- lesson-06/twenty-one.rb
+++ lesson-06/twenty-one.rb
@@ -2,10 +2,9 @@ SPADES = "\u2660"
 HEARTS = "\u2665"
 DIAMONDS = "\u2666"
 CLUBS = "\u2663"
-SUITS = %w(SPADES HEARTS DIAMONDS CLUBS)
+SUITS = [SPADES, HEARTS, DIAMONDS, CLUBS]
 RANKS = %w(A 2 3 4 5 6 7 8 9 J Q K)
 FACE_CARDS = %w(J Q K)
-VALUES = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
 
 def create_deck
   deck = []
@@ -17,9 +16,9 @@ def create_deck
   deck.shuffle!
 end
 
-
 def get_hand_value(hnd)
-  # Remove aces, then replace them at end of hand
+  # Remove aces, then replace them at end of hand.
+  # This reordering is local to this method.
   aces, others = hnd.partition { |card| card[0] == "A" }
   hnd = others + aces
   
@@ -39,7 +38,83 @@ def get_hand_value(hnd)
   end
   value
 end
-    
-hand = [["K", "CLUBS"], ["A", "SPADES"]]
-p get_hand_value(hand)
- 
+
+def print_card(crd)
+  print crd[0],crd[1]
+end
+
+def print_hand(hnd)
+  hnd.each do |card|
+    print_card(card)
+    print " "
+  end
+  puts "\nHand is worth #{get_hand_value(hnd)}"
+end
+
+def display_table(player_hnd, dealer_hnd)
+  system('clear') || system('cls')
+  puts "========="
+  puts "BLACKJACK"
+  puts "========="
+  print "Dealer has: "
+  print_hand(dealer_hnd)
+  puts ""
+  print "You have: "
+  print_hand(player_hnd)
+  puts ""
+end
+
+def get_player_choice
+  loop do
+    puts "Do you want to (h)it or (s)tay?"
+    input = gets.chomp.downcase[0]
+    if input == "h"
+      return "hit"
+    elsif input == "s"
+      return "stay"
+    else
+      puts "Sorry, I didn't understand that."
+    end
+  end
+end
+
+# Main loop
+loop do
+  deck = create_deck
+  
+  player_hand = []
+  dealer_hand = []
+
+  2.times { player_hand <<  deck.shift }
+  1.times { dealer_hand << deck.shift }
+
+  # Player loop
+  loop do
+    display_table(player_hand, dealer_hand)
+
+    choice = get_player_choice
+    if choice == "hit"
+      player_hand << deck.shift
+      next
+    end
+    puts "You stay with #{get_hand_value(player_hand)}"
+    sleep(1.5)
+    break # Player chose stay
+  end
+  
+  # Computer loop
+  display_table(player_hand, dealer_hand)
+  sleep(1.5)
+  loop do
+    puts "Dealer hits"
+    sleep(1.5)
+    display_table(player_hand, dealer_hand)
+    if get_hand_value(dealer_hand) < 17
+      dealer_hand << deck.shift
+    else
+      break # Dealer stays at 17
+    end
+  end
+
+  break
+end
