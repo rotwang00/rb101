3568c3419587ae1fa1ab789eaf4d173a860d195a
Paul Thurmond <paulthurmond@gmail.com>
2021-12-29 07:23:11 -0600

    Created a shuffled deck

--- lesson-06/twenty-one.rb
+++ lesson-06/twenty-one.rb
@@ -1,9 +1,19 @@
-SPADE = "\u2660"
-HEART = "\u2665"
-DIAMOND = "\u2666"
-CLUB = "\u2663"
-BACK = "\u{1f0a0}"
+SPADES = "\u2660"
+HEARTS = "\u2665"
+DIAMONDS = "\u2666"
+CLUBS = "\u2663"
+SUITS = %w(SPADES HEARTS DIAMONDS CLUBS)
+RANKS = %w(A 2 3 4 5 6 7 8 9 J Q K)
+VALUES = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
 
-puts SPADE, HEART, DIAMOND, CLUB, BACK
-puts "K#{HEART}"
-puts "A#{SPADE}"
+def create_deck
+  deck = []
+  RANKS.each do |rank|
+    SUITS.each do |suit|
+      deck << [rank, suit]
+    end
+  end
+  deck.shuffle!
+end
+
+p create_deck
