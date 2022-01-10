74afdf578aa79eb7f8e5da64cd6f44d43ab071fe
Paul Thurmond <paulthurmond@gmail.com>
2022-01-10 17:13:36 -0600

    More practice

---
+++ exercises/word_to_digit.rb
@@ -0,0 +1,22 @@
+NUMBERS = {
+  'one' => '1',
+  'two' => '2',
+  'three' => '3',
+  'four' => '4',
+  'five' => '5',
+  'six' => '6',
+  'seven' => '7',
+  'eight' => '8',
+  'nine' => '9',
+  'zero' => '0'
+}
+
+def word_to_digit(sentence)
+  NUMBERS.keys.each do |word|
+    sentence.gsub!(/\b#{word}\b/, NUMBERS[word])
+  end
+  sentence
+end
+
+puts word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
+
