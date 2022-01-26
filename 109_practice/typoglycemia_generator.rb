0571532bc8c9e62e5cb7c078fd5348c6e1ccbdef
Paul Thurmond <paulthurmond@gmail.com>
2022-01-26 15:27:29 -0600

    More practice

--- 109_practice/typoglycemia_generator.rb
+++ 109_practice/typoglycemia_generator.rb
@@ -30,7 +30,41 @@
 # Algorithm
 # If string is empty, return an empty string
 # Split string into words based on spaces into an array called "words"
-# Map words to a new string called "reversed"
+# Map words to a new string called "alpha"
 # For each word:
 #   Split word into array called "chars"
-#   
+#   Remove and remember the first and last letters. If there is punctuation
+#   at the start or end, remove it too
+#   Alphabatize the interior letters, leaving punctuation in place
+#   Replace the start and end letters, along with any punctuation
+# Return new array
+
+def scramble_words(string)
+  return "" if string == ""
+  words = string.split(" ")
+  alpha = words.map do |word|
+    sorted_word = sort_word(word)
+  end
+end
+
+sort_word(wrd)
+  PUNCTUATION = %w(- ' , .)
+  first = ""
+  
+
+
+end
+
+
+p scramble_words('professionals') == 'paefilnoorsss'
+# p scramble_words('i') == 'i'
+# p scramble_words('') == ''
+# p scramble_words('me') == 'me'
+# p scramble_words('you') == 'you'
+# p scramble_words('card-carrying') == 'caac-dinrrryg'
+# p scramble_words("shan't") == "sahn't"
+# p scramble_words('-dcba') == '-dbca'
+# p scramble_words('dcba.') == 'dbca.'
+# p scramble_words("you've gotta dance like there's nobody watching, love like you'll never be hurt, sing like there's
+# nobody listening, and live like it's heaven on earth.") == "you've gotta dacne like teehr's nbdooy wachintg, love like ylo'ul
+# neevr be hrut, sing like teehr's nbdooy leiinnstg, and live like it's haeevn on earth."
